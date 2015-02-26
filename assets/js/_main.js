df97f4b4a1ce84eee7b5f73457f9c70d29d4fb1a
sohooo <sohooo@intothespirit.com>
2015-02-26 21:29:55 +0100

    floating toc for large displays

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -28,3 +28,40 @@ $(function() {
 	$("article").fitVids();
 });
 
+// Behaviour on Breakpoints
+// conditionally load TOC, on mq @large
+enquire.register("only screen and (min-width: 62.5em)", {
+
+    // OPTIONAL
+    // If supplied, triggered when a media query matches.
+    match : function() {
+      $('#toc').removeClass('hide');
+    },
+
+    // OPTIONAL
+    // If supplied, triggered when the media query transitions
+    // *from a matched state to an unmatched state*.
+    unmatch : function() {
+      $('#toc').addClass('hide');
+    },
+
+    // OPTIONAL
+    // If supplied, triggered once, when the handler is registered.
+    setup : function() {
+      $('#toc').toc();
+      $("#toc").stick_in_parent({
+        parent: 'body',
+        offset_top: 30
+      });
+    },
+
+    // OPTIONAL, defaults to false
+    // If set to true, defers execution of the setup function
+    // until the first time the media query is matched
+    deferSetup : true,
+
+    // OPTIONAL
+    // If supplied, triggered when handler is unregistered.
+    // Place cleanup code here
+    destroy : function() {}
+});
